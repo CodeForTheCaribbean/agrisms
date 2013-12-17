6b6f99d0ed38a93bcf2e1ae85b015349abc38b47
Rory Walker <redrory@gmail.com>
2013-12-17 16:06:15 -0500

    basic find handler added

--- voting/farmer.py
+++ voting/farmer.py
@@ -7,10 +7,11 @@ class FindHandler(KeywordHandler):
 
   def help(self):
     """ Responds with valid commands """
-    self.respond("Valid command: VOTE: [FARMER]")
+    self.respond("Valid command: Find: [FARMER_ID]")
 
   def handle(self, text):
     text = text.strip().lower()
-    data = json.load(urllib2.urlopen('http://localhost:3500/farmers/081117657/.json'))
-    name = data['last_name']
+    data_type = 'http://localhost:3500/farmers/' + text
+    data = json.load(urllib2.urlopen(data_type))
+    name = data['first_name']
     self.respond(name)
